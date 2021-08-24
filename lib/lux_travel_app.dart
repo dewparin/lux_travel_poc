2a2470dc3c9a005504211732e55c7e831300f185
Dew Parin <parinsura@gmail.com>
2021-08-24 11:01:38 +0700

    Lay down bottom navbar.

--- lib/lux_travel_app.dart
+++ lib/lux_travel_app.dart
@@ -2,17 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:lux_travel_poc/screen/home/home_screen.dart';
 
 class LuxTravelApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Lux Travel POC',
       theme: ThemeData.light().copyWith(
         primaryColor: Colors.black,
-        accentColor: Colors.teal,
+        accentColor: Colors.white,
       ),
       home: HomeScreen(),
     );
   }
-
 }
--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _State();
+}
+
+class _State extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Offers',
+    ),
+    Text(
+      'Index 1: My Places',
+    ),
+  ];
+
+  static const List<BottomNavigationBarItem> _bottomNavItems = [
+    BottomNavigationBarItem(
+      icon: Icon(Icons.local_offer),
+      label: 'Offers',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.star_outlined),
+      label: 'My Places',
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Lux'),
       ),
-      body: SafeArea(
-        child: Container(),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _bottomNavItems,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Theme.of(context).accentColor,
+        unselectedItemColor: Colors.white60,
+        backgroundColor: Theme.of(context).primaryColor,
+        onTap: _onItemTapped,
       ),
     );
   }
