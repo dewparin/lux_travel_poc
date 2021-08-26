963dd7e2658a3ef307f08a3b7dbc29c3e71dfef5
Dew Parin <parinsura@gmail.com>
2021-08-26 19:40:53 +0700

    Add OfferDetailScreen.

---
+++ lib/presentation/screens/offer_detail_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OfferDetailScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Offer Detail'),
+    );
+  }
+
+}
