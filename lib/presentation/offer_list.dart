2f3dedec2a7360b783ee49f1d9950c2e375fc386
Dew Parin <parinsura@gmail.com>
2021-08-26 19:22:14 +0700

    Add empty view

--- lib/presentation/offer_list.dart
+++ lib/presentation/offer_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lux_travel_poc/model/offer.dart';
 import 'package:lux_travel_poc/model/offer_model.dart';
 import 'package:lux_travel_poc/presentation/components/offer_item.dart';
+import 'package:lux_travel_poc/presentation/constant.dart';
 import 'package:provider/provider.dart';
 
 class OfferList extends StatelessWidget {
@@ -19,16 +21,44 @@ class OfferList extends StatelessWidget {
         final offers = onlyFavorites
             ? model.allOffers.where((offer) => offer.isFavorite).toList()
             : model.allOffers;
-        return ListView.builder(
-          itemCount: offers.length,
-          itemBuilder: (context, index) => OfferItem(
-            offer: offers[index],
-            onTappedFavorite: (offerUid) {
-              _toggleFavorite(context, offerUid);
-            },
-          ),
-        );
+        if (offers.length > 0) {
+          return _buildListView(offers);
+        } else {
+          return Center(child: _buildEmptyView(context));
+        }
       },
+    );
+  }
+
+  ListView _buildListView(List<Offer> offers) => ListView.builder(
+        itemCount: offers.length,
+        itemBuilder: (context, index) => OfferItem(
+          offer: offers[index],
+          onTappedFavorite: (offerUid) {
+            _toggleFavorite(context, offerUid);
+          },
+        ),
+      );
+
+  Row _buildEmptyView(BuildContext context) {
+    String message;
+    IconData iconData;
+    if (onlyFavorites) {
+      message = "Let's going somewhere!";
+      iconData = Icons.star_border_outlined;
+    } else {
+      message = "No place to go.";
+      iconData = Icons.local_offer_outlined;
+    }
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(iconData),
+        SizedBox(
+          width: smallPadding,
+        ),
+        Text(message),
+      ],
     );
   }
 }
